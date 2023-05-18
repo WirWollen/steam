7738ec808aca883f8519a523bcce12248ac7db4a
WirWollen <52575418+WirWollen@users.noreply.github.com>
2023-05-18 17:20:55 +0400

    Initial commit

---
+++ src/test/java/com/parse/steam/SteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parse.steam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
